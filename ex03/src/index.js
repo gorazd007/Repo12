6c425cba914df4eca3c3d3b4d900c77f1956ca3e
gorazd007 <gorazdtezak@gmail.com>
2022-04-09 10:38:19 +0200

    better

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -11,7 +11,7 @@ function rangeOfNumbers(startN, endN) {
 }
 
 
-console.log(rangeOfNumbers(1, 10));
+console.log(rangeOfNumbers(1, 7));
 console.log(rangeOfNumbers(3, 10));
 console.log(rangeOfNumbers(5, 5));
 console.log(rangeOfNumbers(7, 5));
